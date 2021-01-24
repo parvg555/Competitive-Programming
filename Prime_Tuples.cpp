95d1d4b28575aa7fef9547c10d2bec4a3cfe0108
Parv Gupta <parvg555@gmail.com>
2021-01-24 22:14:17 +0530

    prime tuples

---
+++ Prime_Tuples.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+
+/*=============================================================================
+#  Author:          Parv Gupta - https://github.com/parvg555/
+#  Email:           parvg555@gmail.com
+#  FileName:        PTUPLES
+#  Created On:      24/01/2021
+=============================================================================*/
+const int mxn=1e6+6;
+bool composite[mxn]={0};
+set <int> prime;
+
+void gensieve(){
+    for(int i=2;i<=mxn;i++)
+    {
+        if(composite[i])
+            continue;
+        prime.insert(i);
+        for(int j=2*i;j<=mxn;j+=i)
+            composite[j]=1;
+    }
+}
+
+void sol(){
+    int n;
+    cin>>n;
+    int count = 0; 
+    for(auto x :prime){
+        if(2+x<=n && prime.count(2+x)>0){
+            count++;
+        }
+    }
+    cout<<count<<endl;
+}
+
+int main(void){
+    fast;
+    gensieve();
+    test(t){
+        sol();
+    }
+}
