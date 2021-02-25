c2a14cb7cd400a237adea0411944a7fe510c5d74
Parv Gupta <parvg555@gmail.com>
2021-02-25 23:52:35 +0530

    prime game

--- Prime_Game.cpp
+++ Prime_Game.cpp
@@ -25,7 +25,8 @@ typedef long double  ld;
 #define per(i,a,b) for(ll i=a;i>=b;i--)
 #define remin(a,b) (a=min((a),(b)))
 #define remax(a,b) (a=max((a),(b)))
-
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
 /*=============================================================================
 #  Author:          Parv Gupta - https://github.com/parvg555/
 #  Email:           parvg555@gmail.com
@@ -40,9 +41,9 @@ int prime_before[mxn];
 
 void sieve(){
     for(ll p=2;p*p<=mxn;p++){
-        if(prime[p]==true){
+        if(prime[p]==false){
             for(ll i=p*p;i<=mxn;i+=p){
-                prime[i]=false;
+                prime[i]=true;
             }
         }
     }
@@ -51,7 +52,7 @@ void sieve(){
 void primes(){
     int count = 0;
     for(int i=2;i<=mxn;i++){
-        if(prime[i]==true){
+        if(prime[i]==false){
             count++;
         }
         prime_before[i]=count;
@@ -63,19 +64,19 @@ void sol(){
     cin>>x>>y;
     if(y==1){
         if(x>2){
-            cout<<"Divyam"<<endl;
+            cout<<"Divyam\n";
             return;
         }else{
-            cout<<"Chef"<<endl;
+            cout<<"Chef\n";
             return;
         }
     }else{
         //cout<<"prime counter: "<<prime_counter[x]<<endl;
         if(prime_before[x]>y){
-            cout<<"Divyam"<<endl;
+            cout<<"Divyam\n";
             return;
         }else{
-            cout<<"Chef"<<endl;
+            cout<<"Chef\n";
             return;
         }
     }
@@ -83,12 +84,16 @@ void sol(){
 }
 
 int main(void){
+    //auto start = high_resolution_clock::now();
     fast;
-    memset(prime,true,sizeof(prime));
     sieve();
     primes();
+    prime_before[0] = 0;
+    prime_before[1] = 0;
     test(t){
         sol();
     }
-    
+    //auto stop = high_resolution_clock::now();
+    //auto duration = duration_cast<microseconds>(stop - start);
+    //cout << "Time taken by function: "<< duration.count() << " microseconds" << endl;
 }
