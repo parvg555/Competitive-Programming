c4f39b95de7b3b0618a2797520dca1f48e163400
Parv Gupta <parvg555@gmail.com>
2021-03-25 14:13:02 +0530

    palpal strings

---
+++ Palpal_Strings.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+
+/*=============================================================================
+#  Author:          Parv Gupta - https://github.com/parvg555/
+#  Email:           parvg555@gmail.com
+#  FileName:        Palpal_Strings.cpp
+#  Created On:      27/02/2021
+#  Problem Code:    PALPALS
+=============================================================================*/
+
+
+void sol(){
+    string s;
+    cin>>s;
+    map <char,ll> mp;
+    map <char,ll>::iterator itr;
+    for(ll i=0;i<s.length();i++){
+        mp[s[i]]++;
+    }
+    ll one = 0;
+    ll odd = 0;
+    ll even = 0;
+    for(itr = mp.begin();itr!=mp.end();++itr){
+        //cout<<itr->first<<": "<<itr->second<<"\n";
+        if(itr->second%2!=0){
+            even+=itr->second/2;
+            one++;
+        }else{
+            even+=itr->second/2;
+        }
+    }
+    //cout<<even<<" "<<one<<endl;
+    if(even>=one){
+        cout<<"YES\n";
+    }else{
+        cout<<"NO\n";
+    }
+}
+
+int main(void){
+    fast;
+    test(t){
+        sol();
+    }
+}
