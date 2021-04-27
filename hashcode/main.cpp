363f9e5ebf1b5af69ed1ceb3c7448165391d8266
Parv Gupta <parvg555@gmail.com>
2021-04-28 01:04:36 +0530

    updated

--- hashcode/main.cpp
+++ hashcode/main.cpp
@@ -25,7 +25,8 @@ typedef long double  ld;
 #define remax(a,b) (a=max((a),(b)))
 
 vector < pair<string, tuple<ll,ll,ll>> > intr;// name start end time
-vector<pair<ll,vector<ll>>> cars;// number of streets name of streets
+vector<pair<ll,vector<ll>>> cars;// time path
+vector<pair<ll,vector<pair<string,ll>>>> ans;
 
 ll find_function(string s){
     for(ll i=0;i<intr.size();i++){
@@ -33,6 +34,16 @@ ll find_function(string s){
             return i;
         }
     }
+    return 0;
+}
+
+ll find_streetname(ll e){
+    for(ll i=0;i<intr.size();i++){
+        if(get<1>(intr[i].sec) == e ){
+            return i;
+        }
+    }
+    return 0;
 }
 
 void sol(){
@@ -41,6 +52,7 @@ void sol(){
     ll s; // number of streets
     ll v; //number of cars
     ll f; //bonus for each car that reaches its destination before time d
+    cin>>d>>i>>s>>v>>f;
     fr(i,s){
         ll B,E;//begin and end of intersection
         cin>>B>>E;
@@ -50,19 +62,42 @@ void sol(){
         cin>>time;
         intr.pb(mp(s,make_tuple(B,E,time)));
     }
+    cout<<"input 1 ok\n";
     fr(i,v){
         ll p;
         cin>>p;
         vector <ll> path;
+        ll time = 0;
         fr(j,p){
             string temp;
             cin>>temp;
             ll st = find_function(temp);
+            cout<<get<1>(intr[st].sec)<<"->";/////////////////////////////////////////////////////////////
             path.pb(get<1>(intr[st].sec));
+            if(j!=0){
+                time+=get<2>(intr[st].sec);
+            }
         }
-        cars.pb(mp(p,path));
+        //cout<<"\n";
+        cout<<"\t Journey time: "<<time<<"\n";/////////////////////////////////////////////////////////// 
+        cars.pb(mp(time,path));
     }
-
+    sort(all(cars));
+    ll ans_int=0; //number of intersections in ans
+    fr(i,v){
+        fr(j,cars[i].sec.size()){
+            if(j==0){
+                ll loc = cars[i].sec[j];
+                string street_name = intr[find_streetname(loc)].fi;
+                cout<<street_name<<"\n";
+            }else{
+                ll loc = cars[i].sec[j];
+                string street_name = intr[find_streetname(loc)].fi;
+                cout<<street_name<<"\n";
+            }
+        }
+        cout<<"\n";
+    }
 }
 
 int main(void){
