daa81f8efc9b9c41a6b21589fbcf6ca3053dbc8b
Parv Gupta <parvg555@gmail.com>
2021-06-30 20:49:57 +0530

    codechef

---
+++ Space_Arrays.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+
+/*=============================================================================
+#  Author:          Parv Gupta - https://github.com/parvg555/
+#  Email:           parvg555@gmail.com
+#  FileName:        Space_Arrays.cpp
+#  Created On:      09/03/2021
+#  Problem Code:    SPACEARR
+=============================================================================*/
+
+
+void sol(){
+    ll n;
+    cin>>n;
+    vi arr;
+    fr(i,n){
+        ll temp;
+        cin>>temp;
+        arr.pb(temp);
+    }
+    sort(all(arr));
+    ll count =0;
+    fr(i,n){
+        if(i+1>=arr[i]){
+            count += (i+1)-arr[i];
+        }else{
+            cout<<"Second\n";
+            return;
+        }
+    }
+    if(count%2!=0){
+        cout<<"First\n";
+        return;
+    }
+    cout<<"Second\n";
+}
+
+int main(void){
+    fast;
+    test(t){
+        sol();
+    }
+}
