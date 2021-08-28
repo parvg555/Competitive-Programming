37583e6b4aa25760cc8e1860675ea7c7d6867ea3
Parv <parvg555@gmail.com>
2021-08-28 14:00:00 +0100

    codechef

---
+++ codechef/Team_Formation.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+
+void sol(){
+    int n;
+    cin>>n;
+    string s1,s2;
+    cin>>s1>>s2;
+    int p = 0,e = 0;
+    fr(i,n){
+        if(s1[i] == '1'){
+            p++;
+        }
+        if(s2[i] == '1'){
+            e++;
+        }
+    } 
+    int ans2 = min(p,e);
+    int g=n/2;
+    ans2 = min(g,ans2);
+    cout<<ans2<<"\n";
+}
+
+int main(void){
+    fast;
+    test(t){
+        sol();
+    }
+}
