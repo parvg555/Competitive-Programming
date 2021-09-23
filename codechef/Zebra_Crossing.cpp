3d6cd28c5313ba592edac8f46f336b1ec2c716e8
Parv <parvg555@gmail.com>
2021-09-23 14:00:00 +0100

    codechef

---
+++ codechef/Zebra_Crossing.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define sec second
+typedef long long ll;
+typedef long double  ld;
+#define pii pair<ll,ll>
+#define vi vector< ll >
+#define vvi vector< vi >
+#define vpi vector< pii >
+#define vvpi vector< vpi >
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define test(t) ll t;cin>>t;while(t--)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define inn freopen("input.txt", "r", stdin)
+#define outt freopen("output.txt", "w", stdout)
+#define all(arr) arr.begin(),arr.end()
+#define fr(i,n) for(ll i=0;i<(n);++i)
+#define rep(i,a,b) for(ll i=a;i<=b;++i)
+#define per(i,a,b) for(ll i=a;i>=b;i--)
+#define remin(a,b) (a=min((a),(b)))
+#define remax(a,b) (a=max((a),(b)))
+
+void sol(){
+    ll n,k;
+    cin>>n>>k;
+    string s;
+    cin>>s;
+    ll ans = 0;
+    char current = s[0];
+    for(ll i=1;i<n;i++){
+        if(current!=s[i]){
+            ans++;
+            current = s[i];
+        }
+    }
+    if(ans<k){
+        cout<<"-1\n";
+        return;
+    }
+    if(k%2==0){
+        for(ll i=n-1;i>0;i--){
+            if(s[i]==s[0]){
+                cout<<i+1<<"\n";
+                return;
+            }
+        }
+    }else{
+        for(ll i=n-1;i>0;i--){
+            if(s[i]!=s[0]){
+                cout<<i+1<<"\n";
+                return;
+            }
+        }
+    }
+    
+}
+
+int main(void){
+    fast;
+    test(t){
+        sol();
+    }
+}
